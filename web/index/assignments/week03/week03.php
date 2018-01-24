17f95b1cfe6e2b2576872deeb8c81207f7bb47c3
jsvilchis <vil15009@byui.edu>
2018-01-23 19:31:54 -0700

    Working on week03 team assignment

---
+++ web/index/assignments/week03/week03.php
@@ -0,0 +1,11 @@
+<DOCTYPE! html>
+<head>
+</head>
+<body>
+	Welcome <?php echo $_POST["name"]; ?><br/>
+	Your email address is: <?php echo $_POST["email"]; ?><br/>
+	Your major is <?php echo $_POST["major"]; ?><br/>
+	Comments: <?php echo $_POST["comments"]; ?><br/>
+
+</body>
+</html>
