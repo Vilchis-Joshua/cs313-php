e0e9ffd2a0b57e19451cbd4b84d25e0a94edf301
jsvilchis <vil15009@byui.edu>
2018-01-23 20:04:10 -0700

    Added mailto

--- web/index/assignments/week03/week03.php
+++ web/index/assignments/week03/week03.php
@@ -3,9 +3,9 @@
 </head>
 <body>
 	Name: <?php echo $_POST["fullname"]; ?><br/>
-	Your email address is: <?php echo $_POST["email"]; ?><br/>
+	Email:  <?php echo $_POST["email"]; ?><br/>
+	<a href="mailto:vil15009@byui.edu?Subject=Hello%20again" target="_top">Send mail</a>
 	Your major is <?php echo $_POST["major"]; ?><br/>
 	Comments: <?php echo $_POST["commentsection"]; ?><br/>
-
 </body>
 </html>
