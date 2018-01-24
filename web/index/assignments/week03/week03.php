314a4656899fda1b2b7fd335946c22569bd4f2bc
jsvilchis <vil15009@byui.edu>
2018-01-23 19:48:37 -0700

    Trying to make it work with only one form rather than many

--- web/index/assignments/week03/week03.php
+++ web/index/assignments/week03/week03.php
@@ -5,7 +5,7 @@
 	Welcome <?php echo $_POST["name"]; ?><br/>
 	Your email address is: <?php echo $_POST["email"]; ?><br/>
 	Your major is <?php echo $_POST["major"]; ?><br/>
-	Comments: <?php echo $_POST["comments"]; ?><br/>
+	Comments: <?php echo $_POST["commentssection"]; ?><br/>
 
 </body>
 </html>
