f5b814f49539461d16da081b0d237c8128a94f1b
jsvilchis <vil15009@byui.edu>
2018-01-23 19:54:14 -0700

    Minor adjustments

--- web/index/assignments/week03/week03.php
+++ web/index/assignments/week03/week03.php
@@ -2,7 +2,7 @@
 <head>
 </head>
 <body>
-	Welcome <?php echo $_POST["name"]; ?><br/>
+	Name: <?php echo $_POST["fullname"]; ?><br/>
 	Your email address is: <?php echo $_POST["email"]; ?><br/>
 	Your major is <?php echo $_POST["major"]; ?><br/>
 	Comments: <?php echo $_POST["commentsection"]; ?><br/>
