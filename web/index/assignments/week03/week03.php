6cbc98d7298128a947edbc3b20c0272d42835896
jsvilchis <vil15009@byui.edu>
2018-01-23 19:51:30 -0700

    commented out useless code

--- web/index/assignments/week03/week03.php
+++ web/index/assignments/week03/week03.php
@@ -5,7 +5,7 @@
 	Welcome <?php echo $_POST["name"]; ?><br/>
 	Your email address is: <?php echo $_POST["email"]; ?><br/>
 	Your major is <?php echo $_POST["major"]; ?><br/>
-	Comments: <?php echo $_POST["commentssection"]; ?><br/>
+	Comments: <?php echo $_POST["commentsection"]; ?><br/>
 
 </body>
 </html>
