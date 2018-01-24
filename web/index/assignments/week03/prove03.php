cfebc31b66efeb0aeaa6c4baf797dbc32c02079d
jsvilchis <vil15009@byui.edu>
2018-01-24 14:11:01 -0700

    Added php stuff

---
+++ web/index/assignments/week03/prove03.php
@@ -0,0 +1,11 @@
+<DOCTYPE! html>
+<head>
+</head>
+<body>
+	Name: <?php echo $_POST["fullname"]; ?><br/>
+	Email:  <?php echo $_POST["email"]; ?>	<a href="" target="_top">Send mail</a><br/>
+
+	Your major is <?php echo $_POST["major"]; ?><br/>
+	Comments: <?php echo $_POST["commentsection"]; ?><br/>
+</body>
+</html>
