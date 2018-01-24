3f47fe6941a46b4c5a6fd8aa21766fb3a7d197f5
jsvilchis <vil15009@byui.edu>
2018-01-24 15:18:24 -0700

    Changed the prove03.html h2 tag

--- web/index/assignments/week03/prove03.php
+++ web/index/assignments/week03/prove03.php
@@ -2,10 +2,6 @@
 <head>
 </head>
 <body>
-	Name: <?php echo $_POST["fullname"]; ?><br/>
-	Email:  <?php echo $_POST["email"]; ?>	<a href="" target="_top">Send mail</a><br/>
 
-	Your major is <?php echo $_POST["major"]; ?><br/>
-	Comments: <?php echo $_POST["commentsection"]; ?><br/>
 </body>
 </html>
