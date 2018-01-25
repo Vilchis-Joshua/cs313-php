1fa8f07801652f0922174ce624fa187aebd74ace
jsvilchis <vil15009@byui.edu>
2018-01-24 17:17:45 -0700

    tried to add total

--- web/index/assignments/week03/prove03.js
+++ web/index/assignments/week03/prove03.js
@@ -1,3 +1,11 @@
-function addBacon(amount) {
-
+function addItem(amount) {
+    var price;
+    var total = 0;
+    var tableRow = document.getElementById(amount).firstElementChild.nextElementSibling;
+    while (tableRow != null) {
+        price = parseInt(tableRow.children[1].innerHTML.substr(1));
+        total += tableRow.children[2].firstElementChild.value * price;
+        tableRow = tableRow.nextElementSibling;
+    }
+    document.getElementById("total").innerHTML = total;
 }
