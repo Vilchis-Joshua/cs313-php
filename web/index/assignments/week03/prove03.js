7275f57a96c3ea1173f56c76375d41958c2ec7cc
jsvilchis <vil15009@byui.edu>
2018-01-24 17:30:44 -0700

    fixed typo

--- web/index/assignments/week03/prove03.js
+++ web/index/assignments/week03/prove03.js
@@ -1,16 +1,12 @@
 function addItem(amount) {
     var price;
     var total = 0;
-    var tableRow = document.getElementById(amount).firstElementChild.nextElementSibling;
+    var tableRow = document.getElementById(amount).firstElementChild.firstElementChild.nextElementSibling;
     while (tableRow != null) {
         price = parseInt(tableRow.children[1].innerHTML.substr(1));
-        if (pricea == null) {
-            console.log("Not Working");
-        } else {
-            console.log(price);
-        }
-        total += tableRow.children[3].firstElementChild.value * price;
+
+        total += tableRow.children[2].firstElementChild.value * price;
         tableRow = tableRow.nextElementSibling;
     }
-    document.getElementById("total").innerHTML = total;
+    document.getElementById("total").innerHTML = total
 }
