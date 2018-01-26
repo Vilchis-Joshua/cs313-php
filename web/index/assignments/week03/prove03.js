378836e03580e908eba2b885cda3ae4f637cd65e
jsvilchis <vil15009@byui.edu>
2018-01-26 15:30:34 -0700

    Added the table additin

--- web/index/assignments/week03/prove03.js
+++ web/index/assignments/week03/prove03.js
@@ -1,12 +1,15 @@
 function addItem(amount) {
     var price;
     var total = 0;
-    var tableRow = document.getElementById(amount).firstElementChild.firstElementChild.nextElementSibling;
+    var tableRow = document.getElementById(table).firstElementChild.firstElementChild.nextElementSibling;
     while (tableRow != null) {
         price = parseInt(tableRow.children[1].innerHTML.substr(1));
-
         total += tableRow.children[2].firstElementChild.value * price;
         tableRow = tableRow.nextElementSibling;
     }
-    document.getElementById("total").innerHTML = total
+    document.getElementById("total").innerHTML = total;
+}
+
+function caclulateTotal() {
+
 }
