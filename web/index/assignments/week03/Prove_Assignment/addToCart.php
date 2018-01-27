db772ed624a1e06f19ddff5ef37e9099f9ff204e
jsvilchis <vil15009@byui.edu>
2018-01-26 20:01:02 -0700

    trying7

--- web/index/assignments/week03/Prove_Assignment/addToCart.php
+++ web/index/assignments/week03/Prove_Assignment/addToCart.php
@@ -2,6 +2,7 @@
 	#require("includes/prove03.php");
 	require 'prove03.php';
 	session_start();
+		array_push($_SESSION["product"], $_POST["bacon"]);
 		echo "Bacon quanitity: " .  count($_SESSION["product"]);
            
 ?>
