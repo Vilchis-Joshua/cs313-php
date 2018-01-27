0ad23c5bb83d4082abe56d54bb0df2497e1924c8
jsvilchis <vil15009@byui.edu>
2018-01-26 19:31:20 -0700

    trying1

--- web/index/assignments/week03/Prove_Assignment/prove03.php
+++ web/index/assignments/week03/Prove_Assignment/prove03.php
@@ -1,15 +1,15 @@
 <?PHP
 	session_start();
-
-	function addToCart(value) {
-	if($_SESSION['product']) {
-			$_SESSION['product'] = array();
-			$_SESSION['quantity'] = array();
-		}
-		if (value == 1) {
-			array_push($_SESSION['product'], $_POST['name']);
-		} else {
-			array_push($_SESSION['quantity'], $_POST['number']);
-		}
-	}
+	$_SESSION["Product"] = $_POST["bacon"];
+	#function addToCart(value) {
+	#if($_SESSION['product']) {
+	#		$_SESSION['product'] = array();
+	#		$_SESSION['quantity'] = array();
+	#	}
+	#	if (value == 1) {
+	#		array_push($_SESSION['product'], $_POST['name']);
+	#	} else {
+	#		array_push($_SESSION['quantity'], $_POST['number']);
+	#	}
+	#}
 ?>
