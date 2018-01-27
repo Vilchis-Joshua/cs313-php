f79084cf6f06535cabe58de49d098d827289433c
jsvilchis <vil15009@byui.edu>
2018-01-26 19:39:17 -0700

    trying2

--- web/index/assignments/week03/Prove_Assignment/addToCart.php
+++ web/index/assignments/week03/Prove_Assignment/addToCart.php
@@ -1,9 +1,5 @@
 <?php
 	session_start();
-
-	if(empty($_SESSION['cart'])) {
-		$_SESSION['cart']
-	}
-
-	array_push($_SESSION['cart'], $_POST['id']);
+		echo "Bacon quanitity: " .  count($_SESSION["product"]);
+           
 ?>
