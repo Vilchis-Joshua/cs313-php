4d09d69d76cfe7f49dcd7afac6a33f5fc5aa0f38
jsvilchis <vil15009@byui.edu>
2018-01-31 14:23:37 -0700

    still not working

--- web/index/assignments/week04/TeamActivity/conference.php
+++ web/index/assignments/week04/TeamActivity/conference.php
@@ -1,10 +1,10 @@
 <?PHP
-#	$host = "ec2-54-163-228-190.compute-1.amazonaws.com";
-#	$dbname = "d5s1i23fgv3o18";
-#	$user = "bakrhjfoguxdzg";
-#	$password = "f86428dabd4b102ac30c3ade723ea9b002ff3db70c3eb94bf3740be2b92f9c0c";
-#	$port = "5432";
-#
+	$host = "ec2-54-163-228-190.compute-1.amazonaws.com";
+	$dbname = "d5s1i23fgv3o18";
+	$user = "bakrhjfoguxdzg";
+	$password = "f86428dabd4b102ac30c3ade723ea9b002ff3db70c3eb94bf3740be2b92f9c0c";
+	$port = "5432";
+
 #	$dsn = "pgsql:host=$host;dbname=$dbname;user=$user;port=$port;password=$password";
 #
 #	$db = new PDO($dsn);
@@ -16,7 +16,4 @@
 #	}else {
 #		echo "Not connected";
 #	}
-
-	echo "Username: " . $_POST["username"];
-	echo "Password: " . $_POST["password"]; 
 ?>
