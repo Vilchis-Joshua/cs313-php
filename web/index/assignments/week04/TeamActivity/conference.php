1918ba84b28996cf4ab673e8ebb17feef089ec65
jsvilchis <vil15009@byui.edu>
2018-01-31 14:04:28 -0700

    kk

--- web/index/assignments/week04/TeamActivity/conference.php
+++ web/index/assignments/week04/TeamActivity/conference.php
@@ -1,5 +1,5 @@
 <?PHP
-	$host = "ec2-54-163-228-190.compute-1.amazonaws.com" 
+	$host = "ec2-54-163-228-190.compute-1.amazonaws.com";
 	$dbname = "d5s1i23fgv3o18";
 	$user = "bakrhjfoguxdzg";
 	$password = "f86428dabd4b102ac30c3ade723ea9b002ff3db70c3eb94bf3740be2b92f9c0c";
@@ -10,6 +10,9 @@
 	$db = new PDO($dsn);
 	if($db){
 		echo "Connected <br />".$db;
+		echo "Username: " . $_POST["username"];
+		echo "Password: " . $_POST["password"];
+
 	}else {
 		echo "Not connected";
 }
