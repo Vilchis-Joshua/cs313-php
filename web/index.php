e1d0addb7d2c7e3c43e64e1cfd69599a78d9ab22
jsvilchis <vil15009@byui.edu>
2018-02-07 00:21:42 -0700

    changed the index.php

--- web/index.php
+++ web/index.php
@@ -1,46 +1,12 @@
 <!DOCTYPE html>
 <head>
-	<style>
-		#link {
-			display: flex;
-			align-content: center;
-		}
-	</style>
 </head>
 <body>
 
 <?php
-    $dbUrl = getenv('DATABASE_URL');
+	echo "Hello World!";
 
-    if (empty($dbUrl)) {
-        $dbUrl = "postgres://postgres:hello@127.0.0.1:5433/postgres";
-    }
-
-    $dbopts = parse_url($dbUrl);
-
-    print "<p>$dbUrl</p>\n\n";
-
-    $dbHost = $dbopts["host"];
-    $dbPort = $dbopts["port"];
-    $dbUser = $dbopts["user"];
-    $dbPassword = $dbopts["pass"];
-    $dbName = ltrim($dbopts["path"],'/');
-
-    print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
-
-    try {
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    }
-    catch (PDOException $ex) {
-    print "<p>error: $ex->getMessage() </p>\n\n";
-    die();
-    }
-	foreach ($db->query('SELECT now()') as $row)
-	{
-		print "<p>$row[0]</p>\n\n";
-	}
-
-    ?>
+?>
 
 </body>
 </html>
