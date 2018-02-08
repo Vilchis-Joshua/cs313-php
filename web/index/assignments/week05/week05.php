85570f10c319ba7b9f4f299b1800d12a2eb2682b
jsvilchis <vil15009@byui.edu>
2018-02-07 22:09:18 -0700

    a

--- web/index/assignments/week05/week05.php
+++ web/index/assignments/week05/week05.php
@@ -19,21 +19,20 @@
 		<br />
 		<br />
 	</div>
-	<div>
     <table id="scripts">
 
 	<?php
 	include('php/AccessDb.php');
 		
-	foreach ($db->query('SELECT product_id, product_name, product_price, product_description FROM public.PRODUCT') as $row)
+	foreach ($db->query('SELECT scriptures_id, scriptures_book, scriptures_chapter, scriptures_verse, scriptures_content FROM scripture.scriptures') as $row)
            {
-              echo '<tr id="scriptures_id"> <td><b>' .$row[product_id].' '. $row[product_name].':' .$row[product_price]. '</b> - "' .$row[product_description].'" </td></tr>';
+			  echo "hello";
+              echo '<tr id="scriptures_id"> <td><b>' .$row[scriptures_book].' '. $row[scriptures_chapter].':' .$row[scriptures_verse]. '</b> - "' .$row[scriptures_content].'" </td></tr>';
            }
 		include('php/closeDb.php');
 	?>
 
 	</table>
-	</div>
     <div>
         <a href="../../index.html">Assignment Index</a>
     </div>
