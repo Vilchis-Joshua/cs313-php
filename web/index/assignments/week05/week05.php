3f8f88dce1410f2aca95f4eff862a9d45124bb90
jsvilchis <vil15009@byui.edu>
2018-02-07 21:06:30 -0700

    stuff

--- web/index/assignments/week05/week05.php
+++ web/index/assignments/week05/week05.php
@@ -21,6 +21,12 @@
 		<br />
 	</div
 
+	echo 'table id="scriptures"';
+	foreach ($db->query('SELECT scriptures_id, scriptures_book, scriptures_chapter, scriptures_verse, scriptures_content FROM public.scripture') as $row) {
+		echo '<tr><td>'.$row[scriptures_book] . ' ' . $row[scriptures_chapter] . ':' . $row[scriptures_verse] . $row[scriptures_content] . '</td>';
+	}
+
+	echo '</table>';
     <div>
         <a href="../../index.html">Assignment Index</a>
     </div>
