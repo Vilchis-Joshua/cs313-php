418d3a9f2940fe608540abfb718ea663f7da0f77
jsvilchis <vil15009@byui.edu>
2018-02-07 20:49:18 -0700

    updating

--- web/index/assignments/week05/week05.php
+++ web/index/assignments/week05/week05.php
@@ -9,7 +9,7 @@
     <script src="week03.js"></script>-->
 </head>
 <body>
-	<h2>Hello World!</h2>
+	<h2>Hello World! </h2>
     <div>
         <form action="week05.php">
             Submit: <input name="try" type="submit" />
