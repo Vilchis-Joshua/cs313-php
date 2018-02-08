e9cdc512bd93b56fc141b7af54d5d20f01b63cc1
jsvilchis <vil15009@byui.edu>
2018-02-07 17:28:00 -0700

    updated

--- web/index/assignments/week03/Prove_Assignment/prove03.php
+++ web/index/assignments/week03/Prove_Assignment/prove03.php
@@ -1,18 +1,56 @@
-<?PHP
-	session_start();
-	if (empty($_SESSION["product"])) {
-		$_SESSION["product"] = array();
-	}
-	
-	#function addToCart(value) {
-	#if($_SESSION['product']) {
-	#		$_SESSION['product'] = array();
-	#		$_SESSION['quantity'] = array();
-	#	}
-	#	if (value == 1) {
-	#		array_push($_SESSION['product'], $_POST['name']);
-	#	} else {
-	#		array_push($_SESSION['quantity'], $_POST['number']);
-	#	}
-	#}
-?>
+<?PHP session_start(); ?>
+<?php include('AccessDb.php'); ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Week03 Prove</title>
+    <link rel="stylesheet" type="text/css" href="prove03.css" />
+    <script src="prove03.js"></script>
+</head>
+<body>
+	<?PHP
+		session_start();
+		if (empty($_SESSION["product"])) {
+			$_SESSION["product"] = array();
+		}
+	?>
+
+    <form action="viewcart.php" method="post">
+        <div class="prod">
+            <div id="bacon">
+                <img src="Pictures/bacon.jpg" width="200" height="200" />
+                <input type="checkbox" name="product[]" value="bacon" />
+            </div>
+            <div id="oj">
+                <img src="Pictures/oj.jpg" width="200" height="200" />
+                <input type="checkbox" name="product[]" value="oj" />
+            </div>
+            <div id="hashbrowns">
+                <img src="Pictures/hashbrowns.jpg" width="200" height="200" />
+                <input type="checkbox" name="product[]" value="hashbrowns"/>
+            </div>
+
+            <div id="tables">
+                <img src="Pictures/table.jpg" width="200" height="200" />
+                <input type="checkbox" name="product[]" value="tables" />
+            </div>
+            <div id="chair">
+                <img src="Pictures/chari.jpg" width="200" height="200" />
+                <input type="checkbox" name="product[]" value="chair" />
+            </div>
+            <div id="plates">
+                <img src="Pictures/plate.jpg" width="200" height="200" />
+                <input type="checkbox" name="product[]" value="plates" /> 
+            </div>
+        </div>
+        <input class="inputButton" value="Add" type="submit"/>
+    </form>
+
+
+    <span>
+        <br />
+        <a class="prove" href="../week03.html">Team Activity</a>
+    </span>
+</body>
+</html>
