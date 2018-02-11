7a22bd72a699584c2ac760f079bed206b4873132
jsvilchis <vil15009@byui.edu>
2018-02-10 22:41:25 -0700

    adding the about stuff

---
+++ web/index/assignments/Forum/about.php
@@ -0,0 +1,45 @@
+<?PHP session_start(); ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Week03 Team Activity</title>
+    <link rel="stylesheet" type="text/css" href="css/mainpage.css" />
+    <script src="js/mainpage.js"></script>
+</head>
+<body>
+    <h2>Welcome to CBA</h2>
+
+    <nav id="navigation">
+        <a href="mainpage.php">Main page</a> |
+        <a href="forum.php">Forum</a> |
+        <a href="signin.php">Sign in</a>
+    </nav>
+
+
+
+    <div>
+        <p id="intro">
+            What is CBA? CBA is Cyberbullying Awareness. In current society, there have been more and more problems with bullying <br />
+            on the internet. What are the problems? What are the symptoms of a victim? What can we do? This forum is a place for those involved<br />
+            and for those who wish to help someone that they know if they are suffering, as well as a place to pose new questions and concerns. <br />
+            Nobody here is an actual doctor, so no one should "prescribe" a prescription, but it should be a place of discussion to help get the word out <br />
+            so that we can better work towards a solution.
+        </p>
+    </div>
+
+    <div>
+        <p>
+            Check out some of the most recent articles on cyberbullying
+        </p>
+    </div>
+
+    <div>
+        <p>
+            There will be some articles around here written here.
+        </p>
+    </div>
+    <div class="footer">
+        <a id="footerLink" href="../../index.html">Assignment Index</a>
+    </div>
+</body>
+</html>
