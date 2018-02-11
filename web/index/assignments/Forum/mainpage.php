ea170b0038f3a0dc3ae03c11dceab860f48dde4f
jsvilchis <vil15009@byui.edu>
2018-02-10 21:35:40 -0700

    working?

--- web/index/assignments/Forum/mainpage.php
+++ web/index/assignments/Forum/mainpage.php
@@ -4,32 +4,11 @@
 <html>
 <head>
     <title>Week03 Team Activity</title>
-    <!--<link rel="stylesheet" type="text/css" href="week03.css" />
-    <script src="week03.js"></script>-->
-	<style>
-		#scripts, #scripts td {
-			border: 1px black solid;
-		}
-	</style>
+    <link rel="stylesheet" type="text/css" href="css/mainpage.css" />
+    <script src="js/mainpage.js"></script>
 </head>
 <body>
 	<h2>Hello World! </h2>
-
-	<div>
-		<br />
-		<br />
-	</div>
-    <table id="scripts">
-
-	<?php
-	include('AccessDb.php');
-	
-	$statement = $db->query('SELECT product_name, product_price FROM PRODUCT');
-	$results = $statement->fetchAll(PDO::FETCH_ASSOC);
-	echo $results;
-
-	include('closeDb.php');
-	?>
 	</table>
     <div>
         <a href="../../index.html">Assignment Index</a>
