9113eadf953cdb6d2c0d2d1cfc5b2112ece8d9ff
jsvilchis <vil15009@byui.edu>
2018-02-10 21:44:18 -0700

    update

--- web/index/assignments/Forum/mainpage.php
+++ web/index/assignments/Forum/mainpage.php
@@ -3,40 +3,30 @@
 <html>
 <head>
     <title>Week03 Team Activity</title>
-    <!--<link rel="stylesheet" type="text/css" href="week03.css" />
-    <script src="week03.js"></script>-->
-	<style>
-		#scripts, #scripts td {
-			border: 1px black solid;
-		}
-	</style>
+    <link rel="stylesheet" type="text/css" href="css/mainpage.css" />
+    <script src="js/mainpage.js"></script>
 </head>
 <body>
-	<h2>Hello World! </h2>
+	<h2>Welcome to CBA</h2>
+
+	<div>
+		<p id="intro">
+		   What is CBA? CBA is Cyberbullying Awareness. In current society, there have been more and more problems with bullying <br/>
+		   on the internet. What are the problems? What are the symptoms of a victim? What can we do? This forum is a place for those involved<br/>
+		   and for those who wish to help someone that they know if they are suffering, as well as a place to pose new questions and concerns. <br/>
+		   Nobody here is an actual doctor, so no one should "prescribe" a prescription, but it should be a place of discussion to help get the word out <br />
+		   so that we can better work towards a solution.
+		</p>
+	</div>
 
 	<div>
 		<br />
 		<br />
 	</div>
     <table id="scripts">
-
-	<?php
-	include('AccessDb.php');
-	
-	$statement = $db->query('SELECT product_name, product_price FROM PRODUCT');
-	$results = $statement->fetchAll(PDO::FETCH_ASSOC);
-	echo $results;
-
-	include('closeDb.php');
-	?>
 	</table>
     <div>
         <a href="../../index.html">Assignment Index</a>
     </div>
 </body>
 </html>
-	<?php
-#		foreach ($db->query('SELECT scriptures_id, scriptures_book, scriptures_chapter, scriptures_verse, scriptures_content FROM scripture.scriptures') as $row) {
-#			echo '<tr><td><b>' . $row[scriptures_book] . ' ' . $row[scriptures_chapter] . ':' . $row[scriptures_verse] . '</b> - "' . $row[scriptures_content] . '"</td></tr>';
-#		}	
-#	?>	
