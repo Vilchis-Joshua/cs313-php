134652cda3e9f1bf6b3b6573126db20d72ffe7f1
jsvilchis <vil15009@byui.edu>
2018-02-10 22:27:19 -0700

    main page done

--- web/index/assignments/Forum/mainpage.php
+++ web/index/assignments/Forum/mainpage.php
@@ -7,29 +7,38 @@
     <script src="js/mainpage.js"></script>
 </head>
 <body>
-	<h2>Welcome to CBA</h2>
+    <h2>Welcome to CBA</h2>
 
-	<div>
-		<p id="intro">
-		   What is CBA? CBA is Cyberbullying Awareness. In current society, there have been more and more problems with bullying <br/>
-		   on the internet. What are the problems? What are the symptoms of a victim? What can we do? This forum is a place for those involved<br/>
-		   and for those who wish to help someone that they know if they are suffering, as well as a place to pose new questions and concerns. <br/>
-		   Nobody here is an actual doctor, so no one should "prescribe" a prescription, but it should be a place of discussion to help get the word out <br />
-		   so that we can better work towards a solution.
-		</p>
-	</div>
+    <nav id="navigation">
+        <a href="information.php">Information</a> |
+        <a href="forum.php">Forum</a> |
+        <a href="about.php">About</a> |
+        <a href="signin.php">Sign in</a>
+    </nav>
 
-	<div>
-		<br />
-		<br />
-	</div>
-    <table id="scripts">
-	</table>
     <div>
-		<br /><br />
-		<a href="dummy.html">DUMMY SITE</a>
-		<br /><br />
-        <a href="../../index.html">Assignment Index</a>
+        <p id="intro">
+            What is CBA? CBA is Cyberbullying Awareness. In current society, there have been more and more problems with bullying <br />
+            on the internet. What are the problems? What are the symptoms of a victim? What can we do? This forum is a place for those involved<br />
+            and for those who wish to help someone that they know if they are suffering, as well as a place to pose new questions and concerns. <br />
+            Nobody here is an actual doctor, so no one should "prescribe" a prescription, but it should be a place of discussion to help get the word out <br />
+            so that we can better work towards a solution.
+        </p>
+    </div>
+
+    <div>
+        <p>
+            Check out some of the most recent articles on cyberbullying
+        </p>
+    </div>
+
+    <div>
+        <p>
+            There will be some articles around here written here.
+        </p>
+    </div>
+    <div class="footer">
+        <a id="footerLink" href="../../index.html">Assignment Index</a>
     </div>
 </body>
 </html>
