e9c7783afdace4135f67ca94836043edb36a7357
jsvilchis <vil15009@byui.edu>
2018-02-11 01:11:13 -0700

    updated

---
+++ web/index/assignments/Forum/information.php
@@ -0,0 +1,37 @@
+<?PHP session_start(); ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Week03 Team Activity</title>
+    <link rel="stylesheet" type="text/css" href="css/information.css" />
+    <script src="js/mainpage.js"></script>
+</head>
+<body>
+    <h2>Welcome to CBA</h2>
+
+    <nav id="navigation">
+        <a href="forum.php">Forum</a> |
+        <a href="about.php">About</a> |
+        <a href="signin.php">Sign in</a>
+    </nav>
+
+    <div>
+		<img src="http://resources.uknowkids.com/blog/bid/159164/global-perspective-on-cyberbullying"/>
+    </div>
+
+    <div>
+        <p>
+            Check out some of the most recent articles on cyberbullying
+        </p>
+    </div>
+
+    <div>
+        <p>
+            There will be some articles around here written here.
+        </p>
+    </div>
+    <div class="footer">
+        <a id="footerLink" href="../../index.html">Assignment Index</a>
+    </div>
+</body>
+</html>
