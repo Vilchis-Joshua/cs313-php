96270980937f775294717b01e79412fcb0d3ffd6
jsvilchis <vil15009@byui.edu>
2018-02-13 00:16:40 -0700

    line by line

--- web/index/assignments/Forum/signin.php
+++ web/index/assignments/Forum/signin.php
@@ -48,9 +48,9 @@
 				$password = 'asdf';
 
 				$stmt = $db->prepare('SELECT * FROM user WHERE username=:username AND password=:password');
-				$stmt->bindValue(':username', $username, PDO::PARAM_STR);
-				$stmt->bindValue(':password', $password, PDO::PARAM_STR);
-				stmt->execute();
+#				$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+#				$stmt->bindValue(':password', $password, PDO::PARAM_STR);
+#				stmt->execute();
 #				$rowsChanged = $stmt->rowCount();
 #				$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 #				echo 'Stuff 2 : <br />' . $rowsChanged;
