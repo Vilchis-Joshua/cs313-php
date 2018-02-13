aedb3f8bf2d5e0e4992d21b40d00b04d35d5ed18
jsvilchis <vil15009@byui.edu>
2018-02-13 01:48:58 -0700

    stuff

--- web/index/assignments/Forum/php/article.php
+++ web/index/assignments/Forum/php/article.php
@@ -1,10 +1,10 @@
 <?PHP
-	$stmt = $db->prepare("SELECT article_id, article_title, article_date FROM article");
+	$stmt = $db->prepare("SELECT article_id, article_title, article_date, article_content FROM article");
 	$stmt->execute();
 
 	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 		echo '<p>';
-			echo $row['users_username'] . ' - ' . $row['users_password'];
+			echo $row['article_title'] . ' - ' . $row['article_date'];
 		echo '</p>';				
 	}
 ?>
--- web/index/assignments/Forum/php/user_list.php
+++ web/index/assignments/Forum/php/user_list.php
@@ -2,9 +2,29 @@
 	$stmt = $db->prepare("SELECT users_id, users_username, users_password FROM users");
 	$stmt->execute();
 
+	$users_list = [];
 	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 		echo '<p>';
 			echo $row['users_username'] . ' - ' . $row['users_password'];
-		echo '</p>';				
+		echo '</p>';
+		users_list[] = [
+			'users_id' => $row['users_id'],
+			'users_username' => $row['users_username'],
+			'users_password' => $row['users_password']
+		];
 	}
 ?>
+
+	<!DOCTYPE html>
+	<head>
+	</head>
+	<body>
+		foreach ($users_list as $row) {
+			echo '<p>' 
+			. $row['users_id'] 
+			. ') username - ' . $row['users_username'] . '<br />'
+			. 'password - ' . $row['users_password'] . '<br />'
+			.  '</p>'
+		}
+	</body>
+	</html>
