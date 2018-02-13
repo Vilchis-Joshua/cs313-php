419082696d8cf405e92ca2c34267e334d63ac9ab
jsvilchis <vil15009@byui.edu>
2018-02-13 00:27:55 -0700

    WORKING

--- web/index/assignments/Forum/signin.php
+++ web/index/assignments/Forum/signin.php
@@ -51,9 +51,9 @@
 				$stmt->bindValue(':username', $username, PDO::PARAM_STR);
 				$stmt->bindValue(':password', $password, PDO::PARAM_STR);
 				$stmt->execute();
-#				$rowsChanged = $stmt->rowCount();
-#				$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-#				echo 'Stuff 2 : <br />' . $rowsChanged;
+				$rowsChanged = $stmt->rowCount();
+				$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+				echo 'Stuff :    ' . count($rows, $COUNT_RECURSIVE);
 
 
 
