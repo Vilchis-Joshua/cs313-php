fe0c050eaddbead982b4c5c46ef303940d4ac1b6
jsvilchis <vil15009@byui.edu>
2018-02-19 14:09:55 -0700

    fixed typo

--- web/index/assignments/Forum/forum.php
+++ web/index/assignments/Forum/forum.php
@@ -16,15 +16,16 @@
 
     <div>
 		<?php
-			$stmt = $db->prepare("SELECT discussion_id, discussion_title, discussion_date, discussion_content FROM discussion");
+			$stmt = $db->prepare("SELECT discussion_id, discussion_title, discussion_date, discussion_initial_content FROM discussion");
 			$stmt->execute();
 
 			while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 				echo '<h2>' . $row['discussion_title'] . '</h2><br />';
-				echo '<h3>' . $row['discussion_content'] . '</h3><br />';
-#				echo '<p>';
+				echo '<h3>' . $row['discussion_initial_content'] . '</h3><br />';
+				echo '<p>';
 #				echo $row['users_username'] . ' - ' . $row['users_password'];
-#				echo '</p>';
+				echo 'hello';
+				echo '</p>';
 			}
 		?>
     </div>
