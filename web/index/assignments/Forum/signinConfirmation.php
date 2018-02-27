bb67f33d999295958cdfa933df6152bde29a3091
jsvilchis <vil15009@byui.edu>
2018-02-27 00:45:17 -0500

    a

--- web/index/assignments/Forum/signinConfirmation.php
+++ web/index/assignments/Forum/signinConfirmation.php
@@ -22,35 +22,6 @@
 		$pass = $row['users_password'];
 					echo '1';
 
-		if (password_verify($p, $pass)) {
-			echo '1';
-			$_SESSION['username'] = $un;
-						echo '1';
-
-			header("Location: mainpage.php");
-						echo '1';
-
-			die();
-		} else {
-					echo '1';
-
-			echo 'it failed';
-						echo '1';
-
-		}
-#		if (!isset($_SESSION['userValue']) && empty($_SESSION['userValue']))
-#			
-#			$_SESSION["userValue"] = $_POST['userName'];
-#			echo 'hello';
-#			foreach ($db->query('SELECT * FROM USERS WHERE USERS_ID = $_SESSION['userValue']' as $row)
-#			{
-#				print $row['userValue'];
-#			}
-#		else {
-#			echo 'There is a session';
-#		}
-
-
 	?>
     </div>
 </body>
